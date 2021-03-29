6c6d1b6ca8c41e990166a79a41d04eae5ec14b22
“John <johndoe@example.comclear>
2021-03-29 23:44:29 +0200

    Final

---
+++ file.py
@@ -0,0 +1,20 @@
+import vikingsClasses
+import random
+
+#Saxons
+
+canario = vikingsClasses.Saxon(100, 15)
+unai = vikingsClasses.Saxon(100, 20)
+julieta = vikingsClasses.Saxon(100, 25)
+susana = vikingsClasses.Saxon(100, 50)
+
+#Vikings
+
+manu = vikingsClasses.Viking("Manu", 100, 15)
+solo = vikingsClasses.Viking("solo", 100, 20)
+javier = vikingsClasses.Viking("Javier", 100, 25)
+carmen = vikingsClasses.Viking("Carmen", 100, 40)
+
+vikingsClasses.War.vikingAttack
+vikingsClasses.War.saxonAttack
+vikingsClasses.War.showStatus
