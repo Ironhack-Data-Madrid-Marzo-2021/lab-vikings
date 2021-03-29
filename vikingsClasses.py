3114950ea6a2949512adc8bfc44e71438fdd7dbd
manuleon87 <manuleondominguez@gmail.com>
2021-03-29 17:41:50 +0200

    Test1ok

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,22 +3,39 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
 
-# Viking
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health = self.health - self.damage
+        return
 
 
-class Viking:
-    pass
+'''# Viking
+''class Viking(Soldier): 
+    
+    def __init__(name, health, strength):
+        super().__init__(health, strength))
+        self.name = name
+        self.health = name
+        self.strength = name
+
+#class Viking:
+    
 
 # Saxon
 
 
-class Saxon:
-    pass
+#class Saxon:
+    
 
 # War
 
 
-class War:
-    passs
+#class War:'''
+    
