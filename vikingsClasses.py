adc3b6a51b2d4fb11b5a1cb8921176bd36a9ca69
CarmenRodríguez <carmenrgguaras@hotmail.com>
2021-03-29 21:25:53 +0200

    commit

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -3,22 +3,94 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, damage):
+        self.health -= damage
+        
 
 # Viking
-m
 
-class Viking:
-    pass
+
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage = 1):
+        self.health -= damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+    
+
+    
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage = 1):
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return "A Saxon has died in combat"
+    
+
+
+    
 
 # War
 
 
-class War:
-    pass
+class War():
+    vikingArmy = []
+    saxonArmy = []
+
+    def __init__(self, name, health, strength):
+
+    def addViking(self, Viking = 10):
+        self.Viking += vikingArmy
+
+    def addSaxon(self, Saxon):
+        self.Saxon += saxonArmy
+    
+    def vikingAttack(self):
+        self.saxon -= saxonArmy
+        return receiveDamage(Saxon) == self.strenght(Viking)
+
+    def saxonAttack(self):
+        self.Viking -= vikingArmy
+        return receiveDamage(Viking) == self.strenght(Saxon)
+
+    def showStatus(self):
+        if saxonArmy >
+    
+
+    
+
+
+
+    
+
+        
+
+
+    
