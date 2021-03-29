b9a38b21b703e974c981d285103fab449986f81b
“emiliocarrascomoreno” <emilioemail@gmail.com>
2021-03-29 20:47:03 +0200

    odio las comillas simples que parecen dobles gggrrr

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,24 +1,83 @@
 
-# Soldierr
-
+from random import choice as elige
 
 class Soldier:
-    pass
+    def __init__ (self, health, strength):
+        self.health=health
+        self.strength=strength
 
-# Viking
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self, damage):
+        self.health-=damage
 
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+        self.health = health
+        self.strength = strength
+   
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0 :            
+            return(f"{self.name} has received {damage} points of damage")
+        else:
+            return(f"{self.name} has died in act of combat")
 
-class Viking:
-    pass
+    
+    def battleCry(self):
+        return ("Odin Owns You All!")
 
-# Saxon
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+        self.health = health
+        self.strength = strength
+    
+    def receiveDamage(self, damage):
 
+        self.health -= damage
+        if self.health > 0:
+            return (f"A Saxon has received {damage} points of damage")
+        else:
+            return"A Saxon has died in combat"
 
-class Saxon:
-    pass
+class War(Soldier):
+    def __init__(self):
+        self.vikingArmy=list()
+        self.saxonArmy=list()
+    
+    def addViking(self,Viking):
+        self.vikingArmy.append(Viking)  
+        
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)
 
-# War
+    def vikingAttack(self):
+        herido = elige(self.saxonArmy)
+        atacante = elige(self.vikingArmy)
+        mensaje = herido.receiveDamage(atacante.attack())
+        
+        if mensaje == 'A Saxon has died in combat':
+            self.saxonArmy.remove(herido)   
+        return mensaje
 
+    def saxonAttack(self):
+        herido = elige(self.vikingArmy)
+        atacante = elige(self.saxonArmy)
+        mensaje = herido.receiveDamage(atacante.attack())
 
-class War:
-    pass
+        if mensaje == f"{herido.name} has died in act of combat":
+            self.vikingArmy.remove(herido)  
+        return mensaje
+
+    def showStatus(self):
+        if self.saxonArmy == []:
+            return "Vikings have won the war of the century!"
+        elif self.vikingArmy == [] :
+
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
