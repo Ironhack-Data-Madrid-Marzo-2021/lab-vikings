eb8d89d59796e94f4fd771965cd8338989d81bbb
Luis Sanchez de Leon <luissanchezdeleon@MacBook-Pro-de-Luis-2.local>
2021-03-30 02:07:23 +0200

    No se entiende nada

--- vikingsClasses.py
+++ vikingsClasses.py
@@ -1,24 +1,62 @@
-
+import random
 # Soldier
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    def attack(self):
+        return self.strength
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health = self.health - damage 
 
 # Viking
 
 
-class Viking:
-    pass
-
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.name = name 
+        super().__init__(health, strength)
+    def attack(self):
+        return self.strength
+    def receiveDamage(self,damage):
+        self.damage = damage 
+        self.health = self.health - self.damage
+        if self.health > 0: 
+            return f"{self.name} was dealt {self.damage} damage"
+        if self.health <= 0:
+            return f"{self.name} honorably died in combat." 
+    def battleCry(self): 
+        return "Odin Owns You All!"  
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health,strength)
+    def receiveDamage(self,damage):
+        self.damage = damage
+        self.health = self.health - self.damage
+        if self.health > 0:
+            return f"A Saxon has received {self.damage} damage"
+        if self.health <= 0:
+            return f"A Saxon honorably died in combat." 
 
 # War
 
 
-class War:
-    pass
+class War():
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)
+    def vikingAttack(self):
+        viking = random.choice(self.vikingArmy)
+        saxon = random.choice(self.saxonArmy)
+        damage = saxon.receiveDamage(viking.attack())
+    
